95dea0b2f7133d87dfb99947b66bd1ee2a0bdc4f
MrBochh <jorgealberto1436@gmail.com>
2023-11-24 14:43:10 -0600

    cli script

---
+++ cli-dict.rb
@@ -0,0 +1,27 @@
+require 'sqlite3'
+
+input_word = ARGV 
+
+if input_word.length == 0
+  raise "What do i look up?"
+end
+
+if input_word.length > 1 
+  raise "No more than 1 words"
+end 
+
+# i should add some error handling incase 
+db = SQLite3::Database.open 'dictionary.db'
+
+w = input_word.first .capitalize
+
+query = 'SELECT def FROM words WHERE word = ?'
+result = (db.execute query, w).first
+# They are multiple definitions on certain words, ill just show up the first one 
+db.close
+
+if result.nil?
+  raise "Are you sure you did not misspell?, if not then word is not in database"
+end
+
+puts "\n #{w} \n #{result.first} \n\n"
